b2f4f0a8dab27eb6e8c1e19784b9c1e5c34ccbd7
edenWerke <edenw6292@gmail.com>
2025-02-27 01:09:49 +0300

    Replacing with arrow function

--- script.js
+++ script.js
@@ -110,11 +110,16 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const eurToUsd=1.1;
 
-const movementsUSD= movements.map(function(mov){
-  return mov*eurToUsd;
-  // return 23;
+// const movementsUSD= movements.map(function(mov){
+//   return mov*eurToUsd;
+//   // return 23;
 
-})
+// })
+// replace with arrow
+// const movementsUSD=movements.map(mov=>  mov * eurToUsd)
+const movementsUSD= movements.map(mov=> mov * eurToUsd);
 console.log(movements)
 console.log(movementsUSD)
- 
+const movementUSDfor=[];
+for(const mov of movements) movementUSDfor.push(mov*eurToUsd)
+  console.log(movementUSDfor)
